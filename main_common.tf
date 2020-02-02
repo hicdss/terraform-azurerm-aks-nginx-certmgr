a3b97dd9f28c3385dcdce546a3606b37ae43b71d
Wojciech Puchta <wpu@hicron.com>
2020-02-02 13:45:15 +0100

    hotfix/fix-rg-tags-syntax-error

--- main_common.tf
+++ main_common.tf
@@ -6,14 +6,14 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.PROJECT}${var.INSTANCE}${var.ENVIRONMENT}${random_integer.uuid.result}-rg"
   location = "${var.REGION}"
 
-  tags {
+  tags = {
     project = "${var.PROJECT}"
     instance = "${var.INSTANCE}"
     environment = "${var.ENVIRONMENT}"
   }
 }
 
-resource "random_integer" "uuid" { 
+resource "random_integer" "uuid" {
   min = 100
   max = 999
 }
